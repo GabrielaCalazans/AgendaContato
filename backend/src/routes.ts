24ff19eb291976d7b0a23077dc5a9f7c7c4af2c8
Mateus Moura <mateusvcsmoura@gmail.com>
2025-11-08 17:45:24 -0300

    add: controller layer

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,19 @@
 import { NextFunction, Request, Response, Router } from "express";
+import { ContactsController } from "./controllers/contacts-controller";
+import PrismaContactsRepository from "./repository/prisma/PrismaContactsRepository";
+import ContactsService from "./services/contacts-service";
 
 const router = Router();
 
 router.get('/status', (req: Request, res: Response, next: NextFunction) => {
     return res.status(200).json({ message: "API is online!" });
 });
+
+// Instantiate repository, service and controller (simple DI)
+const repo = new PrismaContactsRepository();
+const service = new ContactsService(repo);
+const contactsController = new ContactsController(service);
+
+router.use('/contacts', contactsController.router);
 
 export { router };
