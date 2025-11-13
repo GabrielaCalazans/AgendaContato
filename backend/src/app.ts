f2fbaa0de9d04a8422a60364fca6af6efbeb9d5b
Mateus Moura <mateusvcsmoura@gmail.com>
2025-11-12 21:01:24 -0300

    feat: install and configure cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,14 @@
 import express from "express";
+import cors from "cors";
 import { router } from "./routes";
 import { errorHandler } from "./middlewares/error-handler";
 
 const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:8100', // porta do Ionic
+  credentials: true
+}));
 
 app.use(express.json());
 app.use('/api', router);
